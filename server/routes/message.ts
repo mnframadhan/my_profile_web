b89744aac375ab2733d7d87920350f08a854a838
mnframadhan <mnfitrahramadhan@gmail.com>
2024-05-30 23:34:48 +0700

    add tanstack router

--- server/routes/message.ts
+++ server/routes/message.ts
@@ -6,19 +6,53 @@ type Message = {
 	message: string
 }
 
-const fake_message: Message[] = [
-		{id: 1, message:"Yo man, great profile!"},
-		{id: 2, message: "Hey dude, lets collab with me"},
-		{id: 3, message: "What a great website, you rock it!"}
-	];
+type Profile = {
+	nama_lengkap: string,
+	nama_panggilan : string,
+	tempat_lahir : string,
+	tanggal_lahir: string,
+	usia: string,
+	pendidikan_terakhir: string,
+	universitas: string,
+	fakultas: string,
+	jurusan: string,
+	masuk_kuliah: number,
+	wisuda: number,
+	email: string,
+	no_hp: string,
+	linkedin: string,
+	cerita_saya: string,
+	gambar: string,
+}
+
+const my_profile: Profile[] = [
+	{
+		nama_lengkap: "Muhammad Nur Fitrah Ramadhan",
+		nama_panggilan: "Fitrah",
+		tempat_lahir: "Pangkep",
+		tanggal_lahir: "17/01/1999",
+		usia: "25",
+		pendidikan_terakhir: "S1",
+		universitas: "Universitas Hasanuddin",
+		fakultas: "Ekonomi dan Bisnis",
+		jurusan: "Ilmu Ekonomi",
+		masuk_kuliah: 2017,
+		wisuda: 2022,
+		email: "fitrah9ramadhan@gmail.com",
+		no_hp: "+62 896-9498-0468",
+		linkedin: "fitrah-ramadhan",
+		cerita_saya: "belum tersedia",
+		gambar: "belum tersedia"
+	}
+]
 
 
 const app = new Hono()
 	.get('/', (c) => {
 
-		const messages = fake_message;
+		const profile_fitrah = my_profile
 
-		return c.json({messages});
+		return c.json({status: 200, message: "success", data: profile_fitrah});
 
 	})
 
