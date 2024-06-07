76ec0d6ddd14797c956b8d46129cbf9981bdbaac
mnframadhan <mnfitrahramadhan@gmail.com>
2024-06-07 19:11:28 +0700

    try: get back db

--- supabase/migrations/0000_early_robbie_robertson.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS "user_profile" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"nama_lengkap" text NOT NULL,
-	"display_name" text NOT NULL,
-	"created_at" date DEFAULT now()
-);
--- supabase/migrations/0003_third_black_crow.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS "request_comp" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"nama_perusahaan" text NOT NULL,
-	"bidang" text NOT NULL,
-	"no_wa" text NOT NULL,
-	"created_at" date DEFAULT now()
-);
---> statement-breakpoint
-CREATE INDEX IF NOT EXISTS "idx_comp" ON "request_comp" USING btree (id);
